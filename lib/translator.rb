c985a6cd2c98eff3222aaad9edb5a2d8281dfc15
Elma Rahman <elmarahman635@gmail.com>
2019-05-02 22:35:47 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,15 +1,39 @@
-require "YAML"
+require 'yaml'
+require 'pry'
 
 # require modules here
 
-def load_library(emoticons)
-  emoticons = YAML.load_file(emoticons.yml)
+def load_library(path) #arg is taking in a string, so it's using whatevers in the .yml file so  we don't need to call it.
+  emoticons = YAML.load_file(path) #we don't have to use path.yml because it's calling . on anything = calling method.
+    #emoji_hash =
+    translator = {
+      get_meaning: {},
+      get_emoticon: {}
+  }
+      emoticons.each do |meaning, emoticon_symbols| #iterate over resulting hash (emoticons)
+          translator[:get_meaning][emoticon_symbols[1]] = meaning #when you make the key you have to set it to something, to make it populate
 
+        binding.pry
+
+        end
+
+        # {
+        #   get_meaning: {
+        #     "☜(⌒▽⌒)☞": "angel"
+        #   },
+        #   get_emoticon: {
+        #     "O:)": "☜(⌒▽⌒)☞"
+        #   }
+        # }
+
+      #binding.pry
   emoticons
-
   # code goes here
-  #Get emoticon, get meaning
+  #Get emoticon, get meaning == point to inner hash
+  #keys -- get meaning = Japanese emoticons, get emoticon - english emoticons
+    #end
 end
+
 
 def get_japanese_emoticon
   # code goes here
